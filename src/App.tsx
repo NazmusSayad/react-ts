6999e70474803583b27d7480f13f3762ca627cb2
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-01-11 21:11:07 +0000

    shifted to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import InitialApp from '$features/_InitialApp/InitialApp'
+import RootLayout from '$layouts/RootLayout'
+import Wrapper from '$layouts/Wrapper'
 
 const App = () => {
-  return <InitialApp />
+  return (
+    <RootLayout>
+      <Wrapper>
+        <h1 className="text-center text-8xl pt-[10vh]">Hello world</h1>
+      </Wrapper>
+    </RootLayout>
+  )
 }
 
 export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,60 @@
+const colors = [
+  '#131517',
+  '#1E1F25',
+  '#282932',
+  '#606389',
+  '#296EFF',
+  '#94b7ff',
+  '#cadbff',
+  '#e2ebff',
+  '#f9fbff',
+]
+
+const lightColorsObj = Object.fromEntries(
+  colors.map((color, ind) => [10 - (ind + 1), color])
+)
+const darkColorsObj = Object.fromEntries(
+  colors.map((color, ind) => [ind + 1, color])
+)
+
+const themeSwapper = require('tailwindcss-theme-swapper')({
+  themes: [
+    {
+      name: 'base',
+      selectors: [':root'],
+      theme: {
+        colors: { clr: lightColorsObj },
+      },
+    },
+    {
+      name: 'dark',
+      selectors: ['.theme-dark'],
+      theme: {
+        colors: { clr: darkColorsObj },
+      },
+    },
+  ],
+})
+
+module.exports = {
+  content: ['./src/**/*.{jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      dm: ['DM Sans', 'sans-serif'],
+    },
+    screens: {
+      xxs: '25em',
+      xs: '31.25em',
+      sm: '37.5em',
+      md: '48em',
+      lg: '62em',
+      xl: '75em',
+      xxl: '120em',
+    },
+  },
+  plugins: [themeSwapper],
+  corePlugins: {
+    preflight: false,
+  },
+}
