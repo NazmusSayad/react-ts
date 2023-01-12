2be3c7abb52f1df2d88c71af007c925be4b5e429
Nazmus Sayad <online2sayad@gmail.com>
2023-01-12 11:04:05 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import store from '$store'
 import getClassName from 'get-classnames'
 
-window.$store = store
-window.$cn = getClassName
-
 declare global {
   var $store: typeof store
   var $cn: typeof getClassName
 }
 
+window.$store = store
+window.$cn = getClassName
 import './index'
