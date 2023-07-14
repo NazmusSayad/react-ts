fccc163b2719ec8fbf2f2059e30e959b95c2e7ca
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-07-14 10:49:22 +0000

    M src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import Wrapper from '$layouts/Wrapper'
-import { useEffect } from 'react'
 
 const App = () => {
-  const jwt = $useStore((state) => state.auth.jwt)
-  console.log(jwt)
-
-  useEffect(() => {
-    $store.auth.login('test')
-  }, [])
-
   return (
     <Wrapper style={{ height: '90%', display: 'grid', placeContent: 'center' }}>
       <h1 style={{ textAlign: 'center', fontSize: '7vw' }}>Hello world!</h1>
